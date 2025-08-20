7cdb71e96fd5d6cc69c4a13dadb04acef512e88a
NithinAIML <nithin.aiml@gmail.com>
2025-08-20 07:09:52 +0530

    Initial commit: COACH Self-Service Portal - AI-powered knowledge management platform

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SelfServicePortal from './components/home/home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SelfServicePortal />
+    </div>
+  );
+}
+
+export default App;
